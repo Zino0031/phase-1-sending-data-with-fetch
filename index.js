174ca6ddaad07682314db09f795e455c8e7166d7
Sidahmed Slikh <sidahmedzinedine@gmail.com>
2023-08-24 22:28:03 +0200

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(username, useremail) {
+    const formData = {
+      name: username,
+      email: useremail
+    };
+  
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+     const result = fetch("http://localhost:3000/users", options)
+      .then(res => res.json())
+      .then(data => { 
+ 
+    document.body.innerHTML = data.id;
+       
+      })
+      .catch(error => {
+        document.body.innerHTML = error.message;
+  
+    });
+    return result;
+}
+
+submitData();
